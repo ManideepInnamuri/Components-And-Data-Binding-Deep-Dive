9d937e67258aa19f0cc69e95678bc0daa0f47809
ManideepInnamuri <56725653+ManideepInnamuri@users.noreply.github.com>
2020-02-10 12:49:49 +0530

    Game Assignment
    Game Assignment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,21 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{type:'server',name:'testserver',content:'Just a test!!'}];
-
-  onServerAdded(serverData:{serverName:string,serverContent:string}) {
-      this.serverElements.push({
-        type: 'server',
-        name: serverData.serverName,
-        content: serverData.serverContent
-      });
+  serverElements = [{ type: 'server', name: 'testserver', content: 'Just a test!!' }];
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
   }
 
-  onABlueprintAdded(blueprintData:{serverName:string,serverContent:string}) {
-      this.serverElements.push({
-        type: 'blueprint',
-        name: blueprintData.serverName,
-        content: blueprintData.serverContent
-      });
+  onABlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
   }
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0)
+    {
+      this.evenNumbers.push(firedNumber);
+    }
+    else{
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+
 }
